dc20109d59274ce024fd098a707d5c2977374750
Paige West <48529882+PaigePalisade@users.noreply.github.com>
2025-04-20 14:26:10 -0400

    rooms

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for
-from flask_socketio import SocketIO
+
+from flask_socketio import SocketIO, join_room
 import json
 
 from sqlalchemy import Integer, String
@@ -11,7 +12,7 @@ from passlib.hash import sha256_crypt
 from PIL import Image
 
 class Base(DeclarativeBase):
-  pass
+    pass
 
 db = SQLAlchemy(model_class=Base)
 
@@ -25,6 +26,7 @@ class Message(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
     body: Mapped[str]
+    room: Mapped[str]
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -32,39 +34,76 @@ class User(db.Model):
     display_name: Mapped[str]
     password: Mapped[str]
 
+class Room(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
 with app.app_context():
     db.create_all()
-
+    
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def landing():
     if 'user_id' not in session:
         return redirect(url_for("login"))
+    if request.method == 'POST':
+        roomname = request.form['room'].lower()
+        error = False
+        if len(roomname) < 1 or len(roomname) > 20:
+            flash("Room name must be between 1 and 20 characters")
+            error = True
+        if not roomname.isalpha:
+            flash("Room name must only contain English letters")
+            error = True
+        if error:
+            return redirect(url_for('landing'))
+        room = Room.query.filter_by(name=roomname).first()
+        if not room:
+            db.session.add(Room(name=roomname))
+            db.session.commit()
+        return redirect(url_for('chat', roomname=roomname))
+        
+        
+    user = User.query.get(session['user_id'])
+    return render_template('index.html', username=user.username, display_name=user.display_name)
+
+@app.route('/room/<roomname>')
+def chat(roomname):
+    roomname = roomname.lower()
+    if 'user_id' not in session:
+        return redirect(url_for("login"))
+    Room.query.filter_by(name=roomname).first_or_404()
     user = User.query.get(session['user_id'])
     if not user:
         session.pop('user_id')
         flash("There was an erorr reading your login")
         return redirect(url_for("login"))
-    return render_template('index.html', user=user)
+    return render_template('chat.html', user=user, roomname=roomname)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username'].lower()
         display_name = request.form['displayname']
         password = request.form['password']
 
         error = False
 
-        if len(username) < 2 or len(username) > 10:
-            flash("Username must be between 2 and 10 characters")
+        if len(username) < 2 or len(username) > 20:
+            flash("Username must be between 2 and 20 characters")
             error = True
-        if len(display_name) < 1 or len(display_name) > 10:
-            flash("Display Name must be between 1 and 10 characters")
+        if len(display_name) < 1 or len(display_name) > 20:
+            flash("Display Name must be between 1 and 20 characters")
             error = True
-        if len(password) < 5 or len(password) > 10:
-            flash("Password must be between 5 and 10 characters")
+        if len(password) < 5 or len(password) > 20:
+            flash("Password must be between 5 and 20 characters")
+            error = True
+        if not username.isalpha():
+            flash("Username must only contain english letters")
+            error = True
+        if User.query.filter_by(username=username).first():
+            flash("Username already exists (not case sensitive)")
             error = True
         if 'profile-picture' in request.files and request.files['profile-picture']:
             file = request.files['profile-picture']
@@ -99,7 +138,7 @@ def logout():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username'].lower()
         password = request.form['password']
 
         error = False
@@ -123,18 +162,74 @@ def login():
                 session['user_id'] = user.id
                 return redirect('/')
         return redirect(url_for('login'))
-
-        session['user_id'] = usr.id
-        return redirect('/')
     return render_template('login.html')
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if not 'user_id' in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        display_name = request.form['displayname']
+        error = False
+        if len(display_name) < 1 or len(display_name) > 20:
+            flash("Display Name must be between 1 and 20 characters")
+            error = True
+        if error:
+            return redirect(url_for('update'))
+        User.query.get(session['user_id']).display_name = display_name
+        db.session.commit()
+        if 'profile-picture' in request.files and request.files['profile-picture']:
+            file = request.files['profile-picture']
+            try:
+                pfp = Image.open(file)
+                pfp.resize((70,70))
+                pfp.save(f"static/pfp/{User.query.get(session['user_id']).username}.webp")
+            except:
+                flash("Could not read pfp")
+                error = True
+        if error:
+            return redirect(url_for('update'))
+        return redirect('/')
+    display_name = User.query.get(session['user_id']).display_name
+    return render_template('update.html', display_name=display_name)
+
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    if not 'user_id' in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        password = request.form['password']
+        new_password = request.form['new-password']
+        user = User.query.get(session['user_id'])
+        error = False
+        if len(new_password) < 5 or len(new_password) > 20:
+            flash("Password must be between 5 and 20 characters")
+            error = True
+        if not sha256_crypt.verify(password, user.password):
+            flash("Current password is invalid")
+            error = True
+        if error:
+            return redirect(url_for('change_password'))
+        new_password = sha256_crypt.hash(new_password)
+        user.password = new_password
+        db.session.commit()
+        return redirect(url_for('update'))
+    return render_template('password.html')
+    
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
-    messages = db.session.execute(db.select(Message))
+
+@socketio.on('getBacklog')
+def get_backlog(roomname):
+    print(roomname)
+    join_room(roomname)
+    messages = db.session.execute(db.select(Message).filter_by(room=roomname))
     chatlog = []
     for msg in messages:
-        chatlog.append({"username": msg[0].username, "message": msg[0].body})
+        display_name = User.query.filter_by(username=msg[0].username).first().display_name
+        chatlog.append({"username": msg[0].username, "body": msg[0].body, 'displayname': display_name})
     socketio.emit('backlog', json.dumps(chatlog), to=request.sid)
     
 
@@ -142,12 +237,20 @@ def handle_connect():
 def handle_message(data):
     print('Received message:', data)
 
-    if 'user_id' in session:
-        msg = Message(username=User.query.get(session['user_id']).username,body=data)
+    obj = json.loads(data)
+    body = obj['body']
+    roomname = obj['roomname']
+
+    if 'user_id' in session and Room.query.filter_by(name=roomname).first():
+        
+        msg = Message(username=User.query.get(session['user_id']).username,body=body, room=roomname)
         db.session.add(msg)
         db.session.commit()
 
-        socketio.emit('newMessage', json.dumps({'username': msg.username, 'message': msg.body}))
+        display_name = User.query.get(session['user_id']).display_name
+
+        socketio.emit('newMessage', json.dumps({'username': msg.username, 'body': msg.body, 'displayname': display_name}), to=roomname)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+    app.run(debug=True)
--- static/chat.js
+++ static/chat.js
@@ -1,32 +1,71 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
+var user = "";
+var roomname = "";
 
 socket.on('connect', function () {
     console.log('Connected to the server');
+    user = document.getElementById("username").textContent;
+    roomname = document.getElementById("roomname").textContent;
+    socket.emit('getBacklog', roomname);
 });
 
 socket.on('backlog', function(data) {
     let arr = JSON.parse(data);
     document.getElementById("chatbody").innerHTML = "";
     for (let i = 0; i < arr.length; i++) {
-        let username = arr[i].username;
-        let message = arr[i].message;
-        const messageParagraph = document.createElement('p');
-        messageParagraph.textContent = username + ": " + message;
-        document.getElementById("chatbody").appendChild(messageParagraph);
+        // let username = arr[i].username;
+        // let body = arr[i].body;
+        // const messageParagraph = document.createElement('p');
+        // messageParagraph.textContent = username + ": " + body;
+        // document.getElementById("chatbody").appendChild(messageParagraph);
+        addMessageToBody(arr[i]);
     }
 });
 
 socket.on('newMessage', function (data) {
     console.log('Server says: ' + data);
     let obj = JSON.parse(data);
-    let username = obj.username;
-    let message = obj.message;
-    const messageParagraph = document.createElement('p');
-    messageParagraph.textContent = username + ": " + message;
-    document.getElementById("chatbody").appendChild(messageParagraph);
+    addMessageToBody(obj);
 });
 
 function sendMessage() {
     var message = document.getElementById('message').value;
-    socket.emit('message', message);
+    socket.emit('message', JSON.stringify({'body': message, 'roomname': roomname}));
+}
+
+function addMessageToBody(obj) {
+    let username = obj.username;
+    let body = obj.body;
+    let display_name = obj.displayname;
+
+    let mainDiv = document.createElement('div');
+    mainDiv.className = 'message';
+    let messageHeader = document.createElement('div');
+    messageHeader.className = 'message-header';
+    let pfpDiv = document.createElement('div');
+    let pfp = document.createElement('img');
+    pfp.className = 'pfp'
+    pfp.src = '/static/pfp/' + username + '.webp';
+    pfpDiv.appendChild(pfp);
+    let nameDiv = document.createElement('div');
+    let displayNameP = document.createElement('p');
+    displayNameP.className = 'display-name';
+    displayNameP.textContent = display_name;
+    let usernameP = document.createElement('p');
+    usernameP.className = 'username';
+    usernameP.textContent = '@' + username;
+    nameDiv.appendChild(displayNameP);
+    nameDiv.appendChild(usernameP);
+    messageHeader.appendChild(pfpDiv);
+    messageHeader.appendChild(nameDiv);
+    let messageBodyP = document.createElement('p');
+    messageBodyP.className = 'message-body';
+    messageBodyP.textContent = body;
+    mainDiv.appendChild(messageHeader);
+    mainDiv.appendChild(messageBodyP);
+    document.getElementById("chatbody").appendChild(mainDiv);
+
+    if (username != user) {
+        mainDiv.classList.add("right");
+    }
 }
